b41af1add82d9ff0c70574b6af720c05c781ec87
itd <itd>
2024-04-23 08:39:22 +0200

    adasdasda

---
+++ gallery.js
@@ -0,0 +1,7 @@
+console.log("kjsjdrf")
+const container = document.querySelector("#container")
+const tomb = ["alma", "körte", "kutya", "macska"]
+
+let ize = document.createElement("div")
+ize.innerText = "blablablablabla"
+container.appendChild(ize)
